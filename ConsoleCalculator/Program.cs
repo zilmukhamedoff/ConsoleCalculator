065cc6ab90ec605a0f9c057dc056786c52650a3b
zilmukhamedoff <askhat.zilmukhamedov@gmail.com>
2024-03-27 13:11:03 -0600

    Add project files.

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator is calculating...");
+        }
+    }
+}
